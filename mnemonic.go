b5418631c519492686cd7e08a869416ca9219116
vedhavyas <vedhavyas.singareddi@gmail.com>
2017-11-23 13:04:50 +0530

    entropy to bits

--- mnemonic.go
+++ mnemonic.go
@@ -3,6 +3,7 @@ package mnemonic
 import (
 	"bytes"
 	"crypto/rand"
+	"crypto/sha256"
 	"fmt"
 )
 
@@ -25,9 +26,9 @@ func toBitsString(data []byte) string {
 }
 
 // contains checks if the int is in provided list
-func contains(c int, l []int) bool {
+func contains(v int, l []int) bool {
 	for _, i := range l {
-		if c == i {
+		if v == i {
 			return true
 		}
 	}
@@ -35,8 +36,8 @@ func contains(c int, l []int) bool {
 	return false
 }
 
-// generateEntropy returns entropy with strength, given strength taken from pre-defined list
-func generateEntropy(strength int) ([]byte, error) {
+// GenerateEntropy returns entropy with strength, given strength taken from pre-defined list
+func GenerateEntropy(strength int) ([]byte, error) {
 	if !contains(strength, acceptedStrengths) {
 		return nil, fmt.Errorf("required strength: %v but got: %d", acceptedStrengths, strength)
 	}
@@ -48,4 +49,20 @@ func generateEntropy(strength int) ([]byte, error) {
 	}
 
 	return b, nil
+}
+
+// getEntropyBits adds required checksum bits to entropy and returns the binary form
+func getEntropyBits(entropy []byte) (string, error) {
+	if !contains(len(entropy), acceptedEntropyLength) {
+		return "", fmt.Errorf("required entropy length: %v but got: %d", acceptedEntropyLength, len(entropy))
+	}
+
+	ckSum := sha256.Sum256(entropy)
+	ckSumBits := toBitsString(ckSum[:])
+	cs := len(entropy) / 32
+
+	var buf bytes.Buffer
+	buf.WriteString(toBitsString(entropy))
+	buf.WriteString(ckSumBits[:cs])
+	return buf.String(), nil
 }
--- mnemonic_test.go
+++ mnemonic_test.go
@@ -30,3 +30,117 @@ func Test_toBitsString(t *testing.T) {
 		}
 	}
 }
+
+func Test_contains(t *testing.T) {
+	tests := []struct {
+		l []int
+		v int
+		r bool
+	}{
+		{
+			l: []int{1, 2, 3},
+			v: 2,
+			r: true,
+		},
+
+		{
+			l: []int{1, 2, 3},
+			v: 4,
+			r: false,
+		},
+
+		{
+			l: nil,
+			v: 2,
+			r: false,
+		},
+
+		{
+			l: []int{1, 2, 3, 2},
+			v: 2,
+			r: true,
+		},
+	}
+
+	for _, c := range tests {
+		r := contains(c.v, c.l)
+		if r != c.r {
+			t.Fatalf("expected %t but got %t for value %d", c.r, r, c.v)
+		}
+	}
+}
+
+func Test_generateEntropy(t *testing.T) {
+	tests := []struct {
+		strength int
+		err      bool
+	}{
+		{
+			strength: 100,
+			err:      true,
+		},
+
+		{
+			strength: 160,
+			err:      false,
+		},
+
+		{
+			strength: 256,
+			err:      false,
+		},
+
+		{
+			strength: 300,
+			err:      true,
+		},
+	}
+
+	for _, c := range tests {
+		d, err := GenerateEntropy(c.strength)
+		if err != nil {
+			if c.err {
+				continue
+			}
+
+			t.Fatalf("unexpected error for strength %d: %v", c.strength, err)
+		}
+
+		if len(d)*8 != c.strength {
+			t.Fatalf("expected %d bits but got %d", c.strength, len(d)*8)
+		}
+	}
+}
+
+func Test_getEntropyBits(t *testing.T) {
+	tests := []struct {
+		entropy []byte
+		result  string
+		err     bool
+	}{
+		{
+			entropy: []byte{157, 139, 19, 71, 158, 100, 239, 50, 20, 107, 23, 98},
+			err:     true,
+		},
+
+		{
+			entropy: []byte{119, 253, 211, 38, 253, 249, 106, 172, 160, 203, 84, 191, 36, 86, 181, 56},
+			result:  "01110111111111011101001100100110111111011111100101101010101011001010000011001011010101001011111100100100010101101011010100111000",
+		},
+	}
+
+	for _, c := range tests {
+		r, err := getEntropyBits(c.entropy)
+		if err != nil {
+			if c.err {
+				continue
+			}
+
+			t.Fatalf("unexpcted error: %v", err)
+		}
+
+		if r != c.result {
+			t.Fatalf("expected %s bits but got %s", c.result, r)
+		}
+	}
+}
